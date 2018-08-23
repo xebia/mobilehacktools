fede04271acb08512e1cf6b4b9214ed5766f74b0
Jeroen Willemsen <jwillemsen@xebia.com>
2018-08-23 15:41:12 +0200

    #29: WIP

--- generic/resources/mobsf.sh
+++ generic/resources/mobsf.sh
@@ -1,6 +1,7 @@
 export JAVA_HOME=/Library/Java/Home
 cd /usr/local/bin/mob-sf/Mobile-Security-Framework-MobSF-master/
 chown -R $(whoami) .
+python3 -m pip install -U --force-reinstall pip
 pip3 install virtualenv
 virtualenv -p python3 venv
 source venv/bin/activate
